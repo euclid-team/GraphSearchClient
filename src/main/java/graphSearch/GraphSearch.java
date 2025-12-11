5b6b6942ab6e65c541e2dba3a19d945169602c93
Pavlos Efraimidis <pefraimi@ee.duth.gr>
2025-12-11 15:26:56 +0200

    GraphSearch 2025 findResults()

--- src/main/java/graphSearch/GraphSearch.java
+++ src/main/java/graphSearch/GraphSearch.java
@@ -19,35 +19,35 @@ public class GraphSearch {
 	}
 
 
-	public Result findResults() {
-		Result res = null;
+    public Result findResults() {
+        Result res = null;
 
-		// ////////////////////
-		// WRITE YOUR OWN CODE
-		// ////////////////////
+        // ////////////////////
+        // WRITE YOUR OWN CODE
+        // ////////////////////
 
-		// EXAMPLE CODE
+        // EXAMPLE CODE
 
-		// Example of creating the Result object
-		res = new Result();
+        // Example of creating the Result object
+        res = new Result();
 
-		// Retrieve the number of clusters (parameter k of maximum spacing clustering)
-		// int numOfClusters = xgraph.getNumOfClusters();
+        // Retrieve the number of clusters (parameter k of maximum spacing clustering)
+        // int numOfClusters = xgraph.getNumOfClusters();
 
-		// Retrieve the first node of the unknown graph
-		long firstNode = xgraph.firstNode();
+        // Retrieve the first node of the unknown graph
+        long firstNode = xgraph.firstNode();
 
-		// Print the ID of the first node
-		System.out.println("The id of the first node is: " + firstNode);
+        // Print the ID of the first node
+        System.out.println("The id of the first node is: " + firstNode);
 
-		// Inform that GraphSearch starts
-		System.out.println("Graph search from node : " + firstNode);
+        // Inform that GraphSearch starts
+        System.out.println("Graph search from node : " + firstNode);
 
-		// Retrieve the neighbors of the first node
-		// The neighbors are given in NO particular order.
-		long[] neighbors = xgraph.getNeighborsOf(firstNode);
+        // Retrieve the neighbors of the first node
+        // The neighbors are given in NO particular order.
+        long[] neighbors = xgraph.getNeighborsOf(firstNode);
 
-		// Convert long[] to ArrayList<Long>
+        // Convert long[] to ArrayList<Long>
 //		Long[] a = new Long[10];
 //		Arrays.fill(a, 123L);
 //		ArrayList<Long> n = new ArrayList<Long>(Arrays.asList(a));
@@ -58,38 +58,37 @@ public class GraphSearch {
 //		    output.add(value);
 //		}
 //
-		// Print all the neighbors of the firstNode
-		// Approach A
-		int numOfNeighbors = neighbors.length;
+        // Print all the neighbors of the firstNode
+        // Approach A
+        int numOfNeighbors = neighbors.length;
 
-		for (int i = 0; i < numOfNeighbors; i++) {
-			System.out.println("Neighbor " + i + ", id: " + neighbors[i]);
-		}
+        for (int i = 0; i < numOfNeighbors; i++) {
+            System.out.println("Neighbor " + i + ", id: " + neighbors[i]);
+        }
 
-		// newline
-		System.out.println();
+        // newline
+        System.out.println();
 
-		// Print all the neighbors of the firstNode
-		// Approach B
-		for (long id : neighbors) {
-			System.out.println("Neighbor id: " + id);
-		}
+        // Print all the neighbors of the firstNode
+        // Approach B
+        for (long id : neighbors) {
+            System.out.println("Neighbor id: " + id);
+        }
 
-		// WRITE ALL RESULTS INTO THE RESULT OBJECT
+        // WRITE ALL RESULTS INTO THE RESULT OBJECT
 
-		// COMPULSORY questions
-		// res.n = (Number of nodes, type: int)
-		// res.m = (Number of edges, type: int)
-		// res.bfsNodeSequence = ... type: ArrayList<Long> (Add Nodes in the Right BFS visiting Order)
-		// res.dfsNodeSequence = ... type: ArrayList<Long> (Add Nodes in the Right DFS visiting Order)
+        // COMPULSORY questions
+        // res.n = (Number of nodes, type: int)
+        // res.m = (Number of edges, type: int)
+        // res.bfsNodeSequence = ... type: ArrayList<Long> (Add Nodes in the Right BFS visiting Order)
+        // res.dfsNodeSequence = ... type: ArrayList<Long> (Add Nodes in the Right DFS visiting Order)
 
-		// BONUS
-		// res.sGraph = ... type: SGraph (This object s used to store the simple undirected weighted graph, nodes SNode and weighted edges SEdge)
-		// res.mst = ... type: TreeSet<SEdge> (The edges of the MST of sGraph)
+        // BONUS
+        // res.sGraph = ... type: SGraph (This object s used to store the simple undirected weighted graph, nodes SNode and weighted edges SEdge)
+        // res.maxDistance (The maximum distance from the first node)
+        // res.graphDiameter (The diameter of the graph, i.e., the maximum distance between any pair of nodes)
 
-		// Return the RESULTS to be sent to the server
-		// Return the res (Result) Object with the results of the computation
-		return res;
-	}
-
+        // Return the res (Result) Object with the results of the computation
+        return res;
+    }
 }
